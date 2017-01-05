6a1d3e9958cd769c12fb0c5c9dfe07c9ed2deab6
Alexey Rogachev <arogachev90@gmail.com>
2017-01-05 17:18:45 +0600

    Closed #28: Portfolio project page - add animation while loading images (#44)

--- assets/js/portfolio_project.js
+++ assets/js/portfolio_project.js
@@ -1,12 +1,16 @@
 $(function () {
     var $screenshotBlock = $('#screenshot');
-    var $screenshot = $screenshotBlock.find('img');
-    var $screenshotLink = $screenshotBlock.find('a');
+    var $screenshotLoading = $screenshotBlock.find('.loading');
+    var $screenshotLink = $screenshotBlock.find('.screenshot-link');
+    var $screenshot = $screenshotBlock.find('.screenshot-image');
 
     var SCREENSHOT_MENU_ITEM_SELECTOR = '.screenshot-menu-item';
     var $screenshotMenuItems = $(SCREENSHOT_MENU_ITEM_SELECTOR);
 
     $screenshotMenuItems.click(function () {
+        $screenshotLoading.show();
+        $screenshotLink.hide();
+
         var $this = $(this);
         var link = $this.data('src');
         var title = $this.data('title');
@@ -18,5 +22,10 @@ $(function () {
             .attr('title', title);
         $screenshotLink.attr('href', link);
         $this.closest(SCREENSHOT_MENU_ITEM_SELECTOR).addClass('active');
+    });
+
+    $screenshot.on('load', function () {
+        $screenshotLoading.hide();
+        $screenshotLink.show();
     });
 });
