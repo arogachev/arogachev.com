c357e78f35b7a23863104630fc5641bb1317ba93
Alexey Rogachev <arogachev90@gmail.com>
2017-01-05 19:47:32 +0600

    Closes #45: Load the first screenshot in portfolio project with animation too

--- assets/js/portfolio_project.js
+++ assets/js/portfolio_project.js
@@ -1,31 +1,46 @@
 $(function () {
+    var SCREENSHOT_MENU_ITEM_SELECTOR = '.screenshot-menu-item';
+    var $screenshotMenuItems = $(SCREENSHOT_MENU_ITEM_SELECTOR);
+    var $screenshotTriggers = $('.screenshot-triggers');
     var $screenshotBlock = $('#screenshot');
     var $screenshotLoading = $screenshotBlock.find('.loading');
     var $screenshotLink = $screenshotBlock.find('.screenshot-link');
-    var $screenshot = $screenshotBlock.find('.screenshot-image');
+    var $screenshot;
 
-    var SCREENSHOT_MENU_ITEM_SELECTOR = '.screenshot-menu-item';
-    var $screenshotMenuItems = $(SCREENSHOT_MENU_ITEM_SELECTOR);
-
-    $screenshotMenuItems.click(function () {
-        $screenshotLoading.show();
-        $screenshotLink.hide();
-
+    $screenshotTriggers.click(function () {
         var $this = $(this);
-        var link = $this.data('src');
+        var link = $this.attr('href');
         var title = $this.data('title');
 
         $screenshotMenuItems.removeClass('active');
+        $this.closest(SCREENSHOT_MENU_ITEM_SELECTOR).addClass('active');
+
+        $screenshotLoading.show();
+        $screenshotLink.hide();
+
+        if (!$screenshot) {
+            $screenshot = $('<img>', {'class': 'img-responsive', alt: title, title: title});
+
+            $screenshot.on('load', function () {
+                $screenshotLoading.hide();
+                $screenshotLink.show();
+            });
+
+            $screenshot.attr('src', link);
+            $screenshotLink.attr('href', link);
+            $screenshotLink.html($screenshot);
+
+            return false;
+        }
+
         $screenshot
             .attr('src', link)
             .attr('alt', title)
             .attr('title', title);
         $screenshotLink.attr('href', link);
-        $this.closest(SCREENSHOT_MENU_ITEM_SELECTOR).addClass('active');
+
+        return false;
     });
 
-    $screenshot.on('load', function () {
-        $screenshotLoading.hide();
-        $screenshotLink.show();
-    });
+    $screenshotTriggers.first().trigger('click');
 });
